06975755ef803212d560e9f8b00f4a6d2b2c9516
Sai Sreenivas Reddy Karra <srivatsava.sai@gmail.com>
2024-07-01 13:37:09 -0700

    Footer component created and added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
+    <>
     <Router>
       <div className="App">
         <Navbar />
@@ -16,6 +18,8 @@ function App() {
         </Routes> */}
       </div>
     </Router>
+    <Footer/>
+    </>
   );
 }
 
